aa995ffb56066ba4579506361928cf4d77969aac
devinminnich <devinminnich1@gmail.com>
2026-08-30 19:18:32 -0600

    Expose undo redo controls

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -2,12 +2,14 @@ import AuthBar from './AuthBar';
 import PersistentPlanner from './PersistentPlanner';
 import ProjectBar from './ProjectBar';
 import ReviewPanel from './ReviewPanel';
+import UndoRedoBar from './UndoRedoBar';
 
 export default function AppShell() {
   return (
     <>
       <AuthBar />
       <ProjectBar />
+      <UndoRedoBar />
       <PersistentPlanner />
       <ReviewPanel />
     </>
