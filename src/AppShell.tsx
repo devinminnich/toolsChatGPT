6d1b5acf093b3b6d5f82d32174ca5ffc765ed518
devinminnich <devinminnich1@gmail.com>
2026-08-30 18:51:27 -0600

    Add application shell with authentication status

---
+++ src/AppShell.tsx
@@ -0,0 +1,11 @@
+import AuthBar from './AuthBar';
+import PersistentPlanner from './PersistentPlanner';
+
+export default function AppShell() {
+  return (
+    <>
+      <AuthBar />
+      <PersistentPlanner />
+    </>
+  );
+}
