a897ec5256c4ffbf630d1d2fa1d4094d69a719ee
devinminnich <devinminnich1@gmail.com>
2026-08-30 18:51:36 -0600

    Mount authenticated application shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import PersistentPlanner from './PersistentPlanner';
+import AppShell from './AppShell';
 import './styles.css';
 import './workspace.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PersistentPlanner />
+    <AppShell />
   </React.StrictMode>,
 );
 
