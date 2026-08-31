9ab97840347f984a3273e97fc990cf209c0826eb
devinminnich <devinminnich1@gmail.com>
2026-08-30 18:50:13 -0600

    Load workspace control styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import PersistentPlanner from './PersistentPlanner';
 import './styles.css';
+import './workspace.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
