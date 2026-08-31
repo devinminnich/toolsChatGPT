96e04a630478899dabff88a7c854fe90deecfc00
devinminnich <devinminnich1@gmail.com>
2026-08-30 18:49:59 -0600

    Promote persistent planner as app entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import PersistentPlanner from './PersistentPlanner';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PersistentPlanner />
   </React.StrictMode>,
 );
 
