35d99cc92abd586ea8dffa1c9c442f0f35e400fc
devinminnich <devinminnich1@gmail.com>
2026-08-30 18:57:12 -0600

    Wire project review panel into application shell

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -1,11 +1,13 @@
 import AuthBar from './AuthBar';
 import PersistentPlanner from './PersistentPlanner';
+import ReviewPanel from './ReviewPanel';
 
 export default function AppShell() {
   return (
     <>
       <AuthBar />
       <PersistentPlanner />
+      <ReviewPanel />
     </>
   );
 }
