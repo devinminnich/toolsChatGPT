2a36f270397526314f652f1b61c79cb9b1b04d59
devinminnich <devinminnich1@gmail.com>
2026-08-30 19:09:58 -0600

    Wire project navigation into app shell

--- src/AppShell.tsx
+++ src/AppShell.tsx
@@ -1,11 +1,13 @@
 import AuthBar from './AuthBar';
 import PersistentPlanner from './PersistentPlanner';
+import ProjectBar from './ProjectBar';
 import ReviewPanel from './ReviewPanel';
 
 export default function AppShell() {
   return (
     <>
       <AuthBar />
+      <ProjectBar />
       <PersistentPlanner />
       <ReviewPanel />
     </>
